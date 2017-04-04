bfd2ef8b1eee2f494e758f4a5089a9674fd1a443
Vincent Rasquier <vrasquier@keley-live.com>
2017-04-04 20:49:43 +0200

    Change flow

--- sdk.js
+++ sdk.js
@@ -1,6 +1,7 @@
 var UPEMConnectSDK = function(userconfig) {
     var defaultconfig = {
         baseUrl: "http://perso-etudiant.u-pem.fr/~vrasquie/cas",
+        iframe: "upem_connect",
         popupHeight: 400,
         popupWidth: 800,
         debug: false
@@ -10,22 +11,35 @@ var UPEMConnectSDK = function(userconfig) {
     this._debug("Init", this.$config);
 }
 
-UPEMConnectSDK.prototype.connect = function connect(callback) {
-    var url = this.$config.baseUrl + "/connect";
-    var popup = this._popup(url, "UPEM Connect");
+UPEMConnectSDK.prototype.init = function init(callback) {
+    var iframe = document.getElementById(this.$config.iframe);
+    if (!iframe) return;
+
+    var self = this;
+    iframe.src = this.$config.baseUrl + "/connect";
+    iframe.style = "border: none;width: 200px;height:70px;";
 
     window.addEventListener("message", function(event) {
-        //if (event.origin !== self.baseUrl)
-            //return;
+        //if (event.origin != window.document.URL) return;
 
         var data = event.data;
         if (!data) {}
         if (!data.token) {}
 
-        //Check token and decrypt
+        var usr = self.extractUsr(data.token);
+        if (!usr) {}
         
-        callback(data.token, data.code);
+        callback(data, usr);
     });
+}
+
+UPEMConnectSDK.prototype.extractUsr = function extractUsr(token) {
+    try {
+        var json = JSON.parse(atob(token.split(".")[1]));
+        return JSON.parse(atob(json.usr));
+    } catch(e) {
+        return null;
+    }
 }
 
 UPEMConnectSDK.prototype._popup = function _popup(url, title) {
