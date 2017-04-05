1cc4e39ac7e25bc06138ea167e49779bf7cce3ad
Vincent Rasquier <vrasquier@keley-live.com>
2017-04-05 15:30:51 +0200

    Better flow

--- sdk.js
+++ sdk.js
@@ -2,8 +2,6 @@ var UPEMConnectSDK = function(userconfig) {
     var defaultconfig = {
         baseUrl: "http://perso-etudiant.u-pem.fr/~vrasquie/cas",
         iframe: "upem_connect",
-        popupHeight: 400,
-        popupWidth: 800,
         debug: false
     };
 
@@ -13,56 +11,31 @@ var UPEMConnectSDK = function(userconfig) {
 
 UPEMConnectSDK.prototype.init = function init(callback) {
     var iframe = document.getElementById(this.$config.iframe);
-    if (!iframe) return;
+    if (!iframe) return this._debug("Error", "No iframe with " + this.$config.iframe);
 
     var self = this;
     iframe.src = this.$config.baseUrl + "/connect";
     iframe.style = "border: none;width: 200px;height:70px;";
 
     window.addEventListener("message", function(event) {
-        //if (event.origin != window.document.URL) return;
-
         var data = event.data;
-        if (!data) {}
-        if (!data.token) {}
+        self._debug("Listener message", data);
 
-        var usr = self.extractUsr(data.token);
-        if (!usr) {}
-        
-        callback(data, usr);
+        if (!data) return self._debug("Error", "No data");
+        if (!data.token) return self._debug("Error", "No token");
+        if (typeof callback != "function") return self._debug("Error", "Callback not a function");
+
+        callback(data);
     });
 }
 
-UPEMConnectSDK.prototype.extractUsr = function extractUsr(token) {
+UPEMConnectSDK.prototype.extractUser = function extractUser(token) {
     try {
         var json = JSON.parse(atob(token.split(".")[1]));
         return JSON.parse(atob(json.usr));
     } catch(e) {
         return null;
     }
-}
-
-UPEMConnectSDK.prototype._popup = function _popup(url, title) {
-    var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
-    var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
-
-    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
-
-    var left = ((width / 2) - (this.$config.popupWidth / 2)) + dualScreenLeft;
-    var top = ((height / 2) - (this.$config.popupHeight / 2)) + dualScreenTop;
-
-    var newWindow = window.open(
-        url, 
-        title, 
-        'scrollbars=yes, width=' + this.$config.popupWidth + ', height=' + this.$config.popupHeight + ', top=' + top + ', left=' + left
-    );
-
-    if (window.focus) {
-        newWindow.focus();
-    }
-
-    return newWindow;
 }
 
 UPEMConnectSDK.prototype._debug = function _debug(action, extra) {
