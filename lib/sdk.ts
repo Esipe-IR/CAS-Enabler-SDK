c14dbe5ee9a51ee71d66a92f55113d370daf25f2
Vincent Rasquier <vrasquier@keley-live.com>
2017-04-19 22:30:30 +0200

    Simplify SDK iteration

--- lib/sdk.ts
+++ lib/sdk.ts
@@ -1,7 +1,6 @@
 export interface Config {
   baseURL: string;
   scope: string;
-  token?: string;
   debug?: boolean;
 };
 
@@ -14,16 +13,21 @@ export interface PostMessage {
   error: string;
 };
 
-export const UPEMSDK_API:string = "UPEM-Api";
+export interface Context {
+  resources: string;
+  date?: string;
+  startDate?: string;
+  endDate?: string;
+};
+
+export const UPEMSDK_API: string = "UPEM-Api";
 export const UPEMSDK_VAULT: string = "UPEM-Vault";
 
-const ACTIONS: any = {
-  RCV_DEFAULT: "receiveDefault",
-  RCV_CONNECT: "receiveConnect",
-  RCV_DISCONNECT: "receiveDisconnect",
-  CONNECT: "askConnect",
-  RESET: "askReset"
-};
+const RCV_DEFAULT = "rcv::default";
+const RCV_CONNECT = "rcv::connect";
+const RCV_DISCONNECT = "rcv::disconnect";
+const ASK_CONNECT = "ask::connect";
+const ASK_RESET = "ask::reset";
 
 export class UPEMSDK {
   private _c: Config;
@@ -40,7 +44,6 @@ export class UPEMSDK {
     let defaultConfig: Config = {
       baseURL: "https://perso-etudiant.u-pem.fr/~vrasquie/core",
       scope: null,
-      token: null,
       debug: false
     }
 
@@ -56,13 +59,13 @@ export class UPEMSDK {
 
     this._callback = {};
     this._f = {
-      [ACTIONS.RCV_CONNECT]: function(msg: PostMessage) {
+      [RCV_CONNECT]: function(msg: PostMessage) {
         self._receiveConnect(msg);
       },
-      [ACTIONS.RCV_DISCONNECT]: function(msg: PostMessage) {
+      [RCV_DISCONNECT]: function(msg: PostMessage) {
         self._receiveDisconnect(msg);
       },
-      [ACTIONS.RCV_DEFAULT]: function(msg: PostMessage) {
+      [RCV_DEFAULT]: function(msg: PostMessage) {
         self._receiveDefault(msg);
       }
     };
@@ -85,30 +88,24 @@ export class UPEMSDK {
       }
 
       self._debug("** Type is not a valid function", {type: msg.type, f: self._f});
-      self._f[ACTIONS.RCV_DEFAULT](msg);
+      self._f[RCV_DEFAULT](msg);
     });
   }
 
   _receiveConnect(msg: PostMessage): void {
     if (this._isValid(msg)) {
-      this._setToken(msg.data);
+      this.setToken(msg.data);
     }
 
     this._receiveDefault(msg);
   }
 
   _receiveDisconnect(msg: PostMessage): void {
-    this._setToken(null);
+    this.setToken(null);
     this._receiveDefault(msg);
   }
 
   _receiveDefault(msg: PostMessage): void {
-    if (Array.isArray(this._callback[msg.type])) {
-      return this._callback[msg.type].forEach(el => {
-        el(msg);
-      });
-    }
-
     if (typeof this._callback[msg.type] === "function") {
       return this._callback[msg.type](msg);
     }
@@ -173,29 +170,17 @@ export class UPEMSDK {
     window.postMessage(msg, "*");
   }
 
-  _checkIfConnect(): void {
+  _check(callback: (data: PostMessage, err: string) => void): void {
     if (!this.getToken()) throw new Error("config.token is undefined. User may not be connected yet");
+    if (typeof callback !== "function") throw new Error("callback should be a function");
   }
 
-  _isValid(msg: PostMessage) {
+  _isValid(msg: PostMessage): boolean {
     if (msg.error === null && msg.code === 0) {
       return true;
     }
 
     return false;
-  }
-
-  _setToken(token: string): void {
-    if (token === this._c.token) return;
-
-    let c = (<any>Object).assign({}, this._c, {
-      token: token
-    });
-
-    this._c = (<any>Object).freeze(c);
-
-    if (!token) localStorage.removeItem("upem-token");
-    else localStorage.setItem("upem-token", this._c.token);
   }
 
   /**
@@ -206,11 +191,7 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   onConnect(callback: (msg: PostMessage) => void, force: boolean): void {
-    if (!this._callback[ACTIONS.RCV_CONNECT]) {
-      this._callback[ACTIONS.RCV_CONNECT] = [];
-    }
-
-    this._callback[ACTIONS.RCV_CONNECT].push(callback);
+    this._callback[RCV_CONNECT] = callback;
     if (force && this.getToken()) this.connect();
   }
 
@@ -222,11 +203,7 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   onDisconnect(callback: (msg: PostMessage) => void): void {
-    if (!this._callback[ACTIONS.RCV_DISCONNECT]) {
-      this._callback[ACTIONS.RCV_DISCONNECT] = [];
-    }
-
-    this._callback[ACTIONS.RCV_DISCONNECT].push(callback); 
+    this._callback[RCV_DISCONNECT] = callback; 
   }
 
   /**
@@ -246,8 +223,8 @@ export class UPEMSDK {
    * 
    * @memberOf UPEMSDK
    */
-  unregisterOnConnect() {
-    this.unregister(ACTIONS.RCV_CONNECT);
+  unregisterOnConnect(): void {
+    this.unregister(RCV_CONNECT);
   }
 
   /**
@@ -256,8 +233,8 @@ export class UPEMSDK {
    * 
    * @memberOf UPEMSDK
    */
-  unregisterOnDisconnect() {
-    this.unregister(ACTIONS.RCV_DISCONNECT);
+  unregisterOnDisconnect(): void {
+    this.unregister(RCV_DISCONNECT);
   }
 
   /**
@@ -268,15 +245,19 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   getToken(): string {
-    if (!this._c.token) {
-      let lStorage = localStorage.getItem("upem-token");
-      
-      if (lStorage) {
-        this._setToken(lStorage);
-      }
-    }
+    return localStorage.getItem("upem-token");
+  }
 
-    return this._c.token;
+  /**
+   * Set Token
+   *
+   * @param {string} token
+   *
+   * @memberOf UPEMSDK
+   */
+  setToken(token: string): void {
+    if (!token) localStorage.removeItem("upem-token");
+    else localStorage.setItem("upem-token", token);
   }
 
   /**
@@ -286,7 +267,7 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   resetVault(): void {
-    this._post(ACTIONS.RESET, null);
+    this._post(ASK_RESET, null);
   }
 
   /**
@@ -301,7 +282,7 @@ export class UPEMSDK {
       token = this.getToken();
     }
 
-    this._post(ACTIONS.RCV_CONNECT, token, "CORE");
+    this._post(RCV_CONNECT, token, "CORE");
   }
 
   /**
@@ -311,7 +292,7 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   disconnect(): void {
-    this._post(ACTIONS.RCV_DISCONNECT, null);
+    this._post(RCV_DISCONNECT, null);
   }
 
   /**
@@ -321,7 +302,7 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   askConnect(): void {
-    this._post(ACTIONS.CONNECT, null);
+    this._post(ASK_CONNECT, null);
   }
 
   /**
@@ -332,7 +313,7 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   getUser(callback: (data: PostMessage, err: string) => void): void {
-    this._checkIfConnect();
+    this._check(callback);
     this._ajax("/me", callback);
   }
 
@@ -344,35 +325,40 @@ export class UPEMSDK {
    * @memberOf UPEMSDK
    */
   getLdapUser(callback: (data: PostMessage, err: string) => void): void {
-    this._checkIfConnect();
+    this._check(callback);
     this._ajax("/me/ldap", callback);
   }
 
   /**
-   * Ajax - Get events for date
+   * Ajax - Get events
    * 
-   * @param {string} date 
+   * @param {Context} ctx 
    * @param {(data: PostMessage, err: string) => void} callback 
    * 
    * @memberOf UPEMSDK
    */
-  getEventsForDate(date: string, callback: (data: PostMessage, err: string) => void): void {
-    this._checkIfConnect();
-    this._ajax("/calendar/events?date=" + date, callback);
-  }
+  getEvents(ctx: Context, callback: (data: PostMessage, err: string) => void): void {
+    this._check(callback);
 
-  /**
-   * Ajax - Get events for range
-   * 
-   * @param {string} start 
-   * @param {string} end 
-   * @param {(data: PostMessage, err: string) => void} callback 
-   * 
-   * @memberOf UPEMSDK
-   */
-  getEventsForRange(start: string, end: string, callback: (data: PostMessage, err: string) => void): void {
-    this._checkIfConnect();
-    this._ajax("/calendar/events?startDate=" + start + "&endDate=" + end, callback);
+    if (!ctx.resources) {
+      throw new Error("ctx.resources is required");
+    }
+
+    var uri = "/calendar/events?resources=" + ctx.resources;
+
+    if (ctx.date) {
+      uri += "&date=" + ctx.date;
+    }
+
+    if (ctx.startDate) {
+      uri += "&startDate=" + ctx.startDate;
+    }
+
+    if (ctx.endDate) {
+      uri += "&endDate=" + ctx.endDate;
+    }
+
+    this._ajax(uri, callback);
   }
 }
 
