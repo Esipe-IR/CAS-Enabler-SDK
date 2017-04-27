b4e3840d94fb891372e368c941dd8f8b7096dbc3
Vincent Rasquier <vrasquier@keley-live.com>
2017-04-27 17:59:59 +0200

    Simplify SDK

--- dist/sdk.d.ts
+++ dist/sdk.d.ts
@@ -11,31 +11,16 @@ export interface PostMessage {
     data: any;
     error: string;
 }
-export interface GraphMessage {
-    query: string;
-    variables?: string;
-}
-export declare const API: string;
+export declare const CORE: string;
 export declare const VAULT: string;
 export declare class SDK {
     private _c;
-    private _f;
     private _callback;
     constructor(userConfig: Config);
     _log(action: string, extra: any): void;
-    _setupReceiver(): void;
-    _isValid(msg: PostMessage): boolean;
-    _ajax(data: GraphMessage, callback: (msg) => void): void;
     _createMsg(type: string, data?: any, code?: number, scope?: string, src?: string, error?: string): PostMessage;
     _post(msg: PostMessage): void;
-    onToken(callback: (msg) => void): SDK;
-    token(): void;
-    unregister(key: string): void;
-    unregisterOnToken(): void;
-    getToken(): string;
-    setToken(token: string): void;
+    onToken(callback: (msg) => void): void;
     resetVault(): void;
     askConnect(): void;
-    getGraph(graph: string, callback: (msg) => void): void;
-    getUser(callback: (msg) => void): void;
 }
--- lib/sdk.ts
+++ lib/sdk.ts
@@ -13,12 +13,7 @@ export interface PostMessage {
   error: string;
 };
 
-export interface GraphMessage {
-  query: string;
-  variables?:string;
-}
-
-export const API: string = "UPEM-Api";
+export const CORE: string = "UPEM-Core";
 export const VAULT: string = "UPEM-Vault";
 
 const RCV_TOKEN = "rcv::token";
@@ -27,7 +22,6 @@ const ASK_RESET = "ask::reset";
 
 export class SDK {
   private _c: Config;
-  private _f: Object;
   private _callback: Object;
 
   /**
@@ -39,14 +33,38 @@ export class SDK {
   constructor(userConfig: Config) {
     let defaultConfig: Config = {
       baseURL: "https://perso-etudiant.u-pem.fr/~vrasquie/core",
-      scope: null,
+      scope: CORE,
       debug: false
-    }
+    };
 
     let c: Config = (<any>Object).assign(defaultConfig, userConfig);
     this._c = (<any>Object).freeze(c);
 
-    this._setupReceiver();
+    var self: SDK = this;
+    this._callback = {};
+
+    window.addEventListener("message", function(event: MessageEvent) {
+      let msg: PostMessage = event.data;
+
+      if (!msg.type) {
+        return self._log("<= EventMessage (RcvError - No Type)", event);
+      }
+
+      if (msg.scope !== self._c.scope) {
+        return self._log("<= EventMessage (RcvError - Bad scopre)", event);
+      }
+
+      if (msg.src == document.URL) {
+        return self._log("=> EventMessage (Send)", msg);
+      }
+
+      self._log("<= Event Message (RCV)", msg);
+
+      if (typeof self._callback[msg.type] === "function") {
+        return self._callback[msg.type](msg.data);
+      }
+    });
+
     this._log("INIT", this);
   }
 
@@ -54,73 +72,6 @@ export class SDK {
     if (this._c.debug) {
       console.log("UPEM SDK", " - " + action + " - ", extra);
     }
-  }
-
-  _setupReceiver(): void {
-    var self: SDK = this;
-
-    this._callback = {};
-    this._f = {
-      [RCV_TOKEN]: function(msg: PostMessage) {
-        self.setToken(msg.data);
-        self.token();
-      }
-    };
-
-    window.addEventListener("message", function(event: MessageEvent) {
-      let msg: PostMessage = event.data;
-
-      if (!msg || !msg.type || msg.scope !== self._c.scope) {
-        return self._log("<= Event Message (RCV - ERROR)", event);
-      }
-
-      if (msg.src == document.URL) {
-        return self._log("=> Event Message (SEND)", msg);
-      }
-
-      self._log("<= Event Message (RCV)", msg);
-
-      if (typeof self._f[msg.type] === "function") {
-        return self._f[msg.type](msg);
-      }
-    });
-  }
-
-  _isValid(msg: PostMessage): boolean {
-    if (msg.error === null && msg.code === 0) {
-      return true;
-    }
-
-    return false;
-  }
-
-  _ajax(data: GraphMessage, callback: (msg) => void): void {
-    var x: XMLHttpRequest = new XMLHttpRequest(); 
-    var url: string = this._c.baseURL + "/graphql";
-    var self: SDK = this;
-
-    this._log("=> AJAX Request", {url: url, token: this.getToken()});
-
-    x.responseType = 'json';
-    x.onreadystatechange = function (oEvent) {
-      if (x.readyState !== 4) {
-        return;
-      }
-
-      self._log("<= AJAX Response", x.response);
-      
-      if (x.response.errors && x.response.errors[0].message === "Invalid token") {
-        self.setToken(null);
-        self.token();
-      }
-
-      callback(x.response);
-    }
-    
-    x.open("POST", url);
-    x.setRequestHeader("Authorization", this.getToken());
-    x.setRequestHeader("Content-Type", "application/json");
-    x.send(JSON.stringify(data));
   }
 
   _createMsg(type: string, data?: any, code?: number, scope?: string, src?: string, error?: string): PostMessage {
@@ -144,64 +95,9 @@ export class SDK {
    * @param {(msg) => void} callback
    * @param {boolean} force 
    */
-  onToken(callback: (msg) => void): SDK {
+  onToken(callback: (msg) => void): void {
     if (typeof callback !== "function") throw new Error("callback should be a function");
-    if (this._callback[RCV_TOKEN]) throw new Error("onToken already defined");
     this._callback[RCV_TOKEN] = callback;
-    
-    return this;
-  }
-
-  /**
-   * Force to execute on token
-   * 
-   * @memberOf SDK
-   */
-  token(): void {
-     this._callback[RCV_TOKEN](this.getToken());
-  }
-
-  /**
-   * Unregister listener by key
-   * 
-   * @param {string} key 
-   * 
-   * @memberOf UPEMSDK
-   */
-  unregister(key: string): void {
-    this._callback[key] = null;
-  }
-
-  /**
-   * Unregister on connect listener
-   * 
-   * @memberOf UPEMSDK
-   */
-  unregisterOnToken(): void {
-    this.unregister(RCV_TOKEN);
-  }
-
-  /**
-   * Get Token
-   * 
-   * @returns {string} 
-   * 
-   * @memberOf UPEMSDK
-   */
-  getToken(): string {
-    return localStorage.getItem("upem-token");
-  }
-
-  /**
-   * Set Token
-   *
-   * @param {string} token
-   *
-   * @memberOf UPEMSDK
-   */
-  setToken(token: string): void {
-    if (!token) localStorage.removeItem("upem-token");
-    else localStorage.setItem("upem-token", token);
   }
 
   /**
@@ -220,46 +116,5 @@ export class SDK {
    */
   askConnect(): void {
     this._post(this._createMsg(ASK_CONNECT));
-  }
-
-  /**
-   * Ajax - Get graph
-   * 
-   * @param {string} graph 
-   * @param {(msg) => void} callback 
-   * 
-   * @memberOf UPEMSDK
-   */
-  getGraph(graph: string, callback: (msg) => void) {
-    if (typeof callback !== "function") throw new Error("callback should be a function");
-
-    var msg: GraphMessage = {
-      query: graph
-    };
-
-    this._ajax(msg, callback);
-  }
-
-  /**
-   * Ajax - Get user
-   * 
-   * @param {(msg) => void} callback 
-   * 
-   * @memberOf UPEMSDK
-   */
-  getUser(callback: (msg) => void): void {
-    if (!this.getToken()) throw new Error("config.token is undefined. User may not be connected yet");
-    
-    var graph = `query {
-      user {
-        uid
-        name
-        lastname
-        email
-        class
-      }
-    }`;
-
-    this.getGraph(graph, callback);
   }
 }
